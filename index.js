dfd6a20f327eb7667d7fed804560f113fef474e2
Zacharia Ndinguri <zacthuku@gmail.com>
2025-03-21 11:02:06 +0300

    liker lab

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Your code here
+// Your code
+// Function 1: Create an Employee Record
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  // Function 2: Create Multiple Employee Records
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(record => createEmployeeRecord(record));
+  }
+  
+  // Function 3: Create Time In Event
+  function createTimeInEvent(employeeRecord, dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date
+    });
+    return employeeRecord;
+  }
+  
+  
+  function createTimeOutEvent(employeeRecord, dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date
+    });
+    return employeeRecord;
+  }
+  
+ e
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeIn = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOut = employeeRecord.timeOutEvents.find(event => event.date === date);
+  
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    return hoursWorked * employeeRecord.payPerHour;
+  }
+  
+ 
+  function allWagesFor(employeeRecord) {
+    const datesWorked = employeeRecord.timeInEvents.map(event => event.date);
+    return datesWorked.reduce((total, date) => total + wagesEarnedOnDate(employeeRecord, date), 0);
+  }
+  
+
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, record) => total + allWagesFor(record), 0);
+  }
+  
